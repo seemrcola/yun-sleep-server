db6197facd4d157b2356e83de7adbfc6b9ab3479
seemr <110106669+seemrcola@users.noreply.github.com>
2025-05-04 01:47:50 +0800

    init

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,6 +1,7 @@
 import { MidwayConfig } from '@midwayjs/core';
 import { User } from '../entity/user.entity';
 import { Room } from '../entity/room.entity';
+import 'dotenv/config';
 
 export default {
   // use for cookie sign key, should change to your own and keep security
@@ -12,7 +13,7 @@ export default {
     dataSource: {
       default: {
         type: 'postgres',
-        url: 'postgresql://neondb_owner:npg_je3DKtcEk5Or@ep-solitary-sky-a4dj7izq-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require',
+        url: process.env.POSTGRES_URL,
         entities: [User, Room],
         synchronize: true, // 自动同步数据库结构，生产环境建议关闭
         logging: true, // 打印数据库查询日志
@@ -20,7 +21,7 @@ export default {
     },
   },
   jwt: {
-    secret: 'your-jwt-secret-key', // 在实际应用中应该使用环境变量
+    secret: process.env.JWT_SECRET, // 在实际应用中应该使用环境变量
     expiresIn: '24h',
   },
 } as MidwayConfig;
